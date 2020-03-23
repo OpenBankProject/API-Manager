fdd80d878331c414bddd64724b4ef879a446ab28
hongwei <hongwei@tesobe.com>
2020-03-23 13:57:19 +0100

    generate the jsoneditor for all method roughings

--- apimanager/methodrouting/static/methodrouting/js/methodrouting.js
+++ apimanager/methodrouting/static/methodrouting/js/methodrouting.js
@@ -1,62 +1,66 @@
-const schema = {};
-var json = [
-	{
-		key: 'url',
-		value: 'http://localhost:8080'
-	},
-	{
-		"key":"outBoundMapping",
-		"value":{
-			"cc":{
-				"cId":"outboundAdapterCallContext.correlationId"
-			},
-			"bankId":"bankId.value + 'helloworld'",
-			"originalJson":"$root"
-		}
-	},
-	{
-		"key":"inBoundMapping",
-		"value":{
-			"inboundAdapterCallContext$default":{
-				"correlationId":"correlation_id_value",
-				"sessionId":"session_id_value"
-			},
-			"status$default":{
-				"errorCode":"",
-				"backendMessages":[]
-			},
-			"data":{
-				"bankId":{
-					"value":"result.bank_id"
+$(document).ready(function($) {
+	const schema = {};
+	var json = [
+		{
+			key: 'url',
+			value: 'http://localhost:8080'
+		},
+		{
+			"key":"outBoundMapping",
+			"value":{
+				"cc":{
+					"cId":"outboundAdapterCallContext.correlationId"
 				},
-				"shortName":"result.name",
-				"fullName":"'full: ' + result.name",
-				"logoUrl":"result.logo",
-				"websiteUrl":"result.website",
-				"bankRoutingScheme[0]":"result.routing.routing_scheme",
-				"bankRoutingAddress[0]":"result.routing.routing_address",
-				"swiftBic":"result.swift_bic",
-				"nationalIdentifier":"result.national"
+				"bankId":"bankId.value + 'helloworld'",
+				"originalJson":"$root"
 			}
-		}
-	}];
+		},
+		{
+			"key":"inBoundMapping",
+			"value":{
+				"inboundAdapterCallContext$default":{
+					"correlationId":"correlation_id_value",
+					"sessionId":"session_id_value"
+				},
+				"status$default":{
+					"errorCode":"",
+					"backendMessages":[]
+				},
+				"data":{
+					"bankId":{
+						"value":"result.bank_id"
+					},
+					"shortName":"result.name",
+					"fullName":"'full: ' + result.name",
+					"logoUrl":"result.logo",
+					"websiteUrl":"result.website",
+					"bankRoutingScheme[0]":"result.routing.routing_scheme",
+					"bankRoutingAddress[0]":"result.routing.routing_address",
+					"swiftBic":"result.swift_bic",
+					"nationalIdentifier":"result.national"
+				}
+			}
+		}];
 
-const options = {
-	mode: 'code',
-	modes: ['code', 'text', 'tree', 'preview']
-};
-// get json
-function getJSON() {
-	var json = editor.get();
-	alert(JSON.stringify(json, null, 2));
-}
-// create the editor
-const container = document.getElementById('jsoneditor');
-const editor = new JSONEditor(container, options, json);
+	const options = {
+		mode: 'code',
+		modes: ['code', 'text', 'tree', 'preview']
+	};
+	
+	var jsoneditorNumber = $('.jsoneditor_div').length
+	for (var step = 0; step < jsoneditorNumber; step++) {
+		var container = $("#"+"jsoneditor"+step);
+		new JSONEditor(container[0], options, json);
+	}
+	
 // // get json
 // const updatedJson = editor.get()
 
-$(document).ready(function($) {
+	$('.parameters').click(function() {
+		var row = $(this).parent().parent();
+		row.find('.jsoneditor_div').css("display","block");
+	});
+	
 	$('.runner button.forSave').click(function() {
 		var t = $(this);
 		var runner = $(this).parent().parent().parent();
@@ -80,6 +84,7 @@ $(document).ready(function($) {
 		}, function (response) {
 			location.reload(); 
 		});
+		runner.find('jsoneditor_div').css("display","none");
 		return false;
 	});
 
