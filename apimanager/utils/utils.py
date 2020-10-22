62189a95fd1c9463e9bc104af2ac5341f5736616
Hongwei <hongwei@tesobe.com>
2020-10-22 14:04:58 +0200

    feature/used the utils and put the get_cache_key_for_current_call method there

--- apimanager/utils/utils.py
+++ apimanager/utils/utils.py
@@ -5,6 +5,7 @@ Base utilities
 
 from datetime import datetime
 from django.contrib.humanize.templatetags.humanize import naturaltime
+from base import context_processors
 
 
 def json_serial(obj):
@@ -14,3 +15,9 @@ def json_serial(obj):
         serial = naturaltime(obj)
         return serial
     raise TypeError('Type not serializable')
+
+def get_cache_key_for_current_call(request, urlpath):
+    """we will generate the cache key by login username+urlpath
+       url path may contain lots of special characters, here we use the hash method first.
+    """
+    return context_processors.api_username(request).get('API_USERNAME') + str(hash(urlpath)) 
