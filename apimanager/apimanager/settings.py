026920053430966bbd2c35757855f77af9aee3ac
Reena-cell <ranireena68@gmail.com>
2022-09-29 15:37:49 +0200

    /feature Add Product List Page

--- apimanager/apimanager/settings.py
+++ apimanager/apimanager/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'bootstrap3',
     'bootstrap_datepicker_plus',
     'mathfilters',
-
     'base',
     'obp',
     'consumers',
@@ -56,6 +55,7 @@ INSTALLED_APPS = [
     'branches',
     'atms',
     'atmlist',
+    'productlist',
     'entitlementrequests',
     'customers',
     'metrics',
