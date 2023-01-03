2f4e2dc48a127ca9d5fb89c73e033b647feb8012
Reena-cell <ranireena68@gmail.com>
2023-01-03 12:41:17 +0100

    feature/ add update button in api-collection

--- apimanager/apicollections/static/apicollections/js/apicollections.js
+++ apimanager/apicollections/static/apicollections/js/apicollections.js
@@ -5,7 +5,7 @@ $(document).ready(function($) {
         let api_collection_name = $(runner).find('.api_collection_name').val();
         let api_collection_is_sharable = $(runner).find('.api_collection_is_sharable').val();
         let api_collection_description = $(runner).find('.api_collection_description').val();
-    
+        $('.runner button.forUpdate').attr("disabled","disabled");
 		$('.runner button.forSave').attr("disabled","disabled");
 		$('.runner button.forDelete').attr("disabled","disabled");
 		$.post('save/apicollection', {
@@ -16,11 +16,31 @@ $(document).ready(function($) {
 			location.reload(); 
 		});
     });
+    $('.runner button.forUpdate').click(function(e) {
+        e.preventDefault();
+        let runner = $(this).parent().parent().parent();
+        let api_collection_id = $(runner).find('.api_collection_id').attr("value");
+        let api_collection_name = $(runner).find('.api_collection_name').val();
+        let api_collection_is_sharable = $(runner).find('.api_collection_is_sharable').val();
+        let api_collection_description = $(runner).find('.api_collection_description').val();
+        $('.runner button.forUpdate').attr("disabled","disabled");
+        $('.runner button.forSave').attr("disabled","disabled");
+        $('.runner button.forDelete').attr("disabled","disabled");
+        $.post('update/apicollection', {
+                'api_collection_id': api_collection_id,
+                'api_collection_name': api_collection_name,
+                'api_collection_is_sharable': api_collection_is_sharable,
+                'api_collection_description': api_collection_description,
+            }, function (response) {
+                location.reload();
+        });
+    });
 
     $('.runner button.forDelete').click(function(e) {
 		e.preventDefault();
         let runner = $(this).parent().parent().parent();
-        let api_collection_id = $(runner).find('.api_collection_id').html();
+        let api_collection_id = $(runner).find('.api_collection_id').attr("value");
+        $('.runner button.forUpdate').attr("disabled","disabled");
 		$('.runner button.forSave').attr("disabled","disabled");
 		$('.runner button.forDelete').attr("disabled","disabled");
 		$.post('delete/apicollection', {
