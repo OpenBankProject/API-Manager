d99763c223097507c3275cd21e301234b3e380c8
Reena-cell <ranireena68@gmail.com>
2023-01-26 14:35:00 +0100

    update Date and time Variables name

--- apimanager/metrics/views.py
+++ apimanager/metrics/views.py
@@ -709,7 +709,8 @@ class MonthlyMetricsSummaryView(LoginRequiredMixin, TemplateView):
         return top_apps_using_warehouse
 
     def median_time_to_first_api_call(self, from_date, to_date):
-        return 0
+        return 0 #TODO this cost too much time, do not use this at the moment.
+        form = self.get_form()
 
     def get_context_data(self, **kwargs): return self.prepare_general_context(SummaryType.MONTHLY)
 
