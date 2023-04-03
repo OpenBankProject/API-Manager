4e0333d804d8b70c6d8364041eae706be2c7b370
Reena  Aheer <ranireena68@gmail.com>
2023-04-03 07:16:19 +0200

    Add attribute fileds in form

--- apimanager/atms/forms.py
+++ apimanager/atms/forms.py
@@ -206,14 +206,14 @@ class CreateAtmForm(forms.Form):
             required=False,
        )
     has_deposit_capability = forms.ChoiceField(
-             label=_('Deposit Capabilities'),
-             widget=forms.Select(
-                  attrs={
-                         'class': 'form-control',
-                  }
-             ),
-             required=False,
-        )
+         label=_('Deposit Capabilities'),
+         widget=forms.Select(
+              attrs={
+                     'class': 'form-control',
+              }
+         ),
+         required=False,
+    )
     supported_languages = forms.CharField(
             label=_('Supported Languages'),
             widget=forms.TextInput(
@@ -263,6 +263,36 @@ class CreateAtmForm(forms.Form):
         ),
         required=False,
     )
+    type_attribute = forms.CharField(
+        label=_('Type'),
+        widget=forms.TextInput(
+            attrs={
+               'placeholder': _('Type'),
+               'class': 'form-control',
+            }
+        ),
+        required=False,
+    )
+    name_attribute = forms.CharField(
+        label=_('Attribute Name'),
+        widget=forms.TextInput(
+            attrs={
+               'placeholder': _('Name'),
+               'class': 'form-control',
+            }
+        ),
+        required=False,
+    )
+    value_attribute = forms.CharField(
+            label=_('Value'),
+            widget=forms.TextInput(
+                attrs={
+                   'placeholder': _('2012-04-23'),
+                   'class': 'form-control',
+                }
+            ),
+            required=False,
+        )
     location_categories = forms.CharField(
         label=_('Location Category'),
         widget=forms.TextInput(
