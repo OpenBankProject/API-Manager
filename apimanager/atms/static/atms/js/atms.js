447a15007529b00408da11f95b0dbf47658f4a63
Reena  Aheer <ranireena68@gmail.com>
2023-04-03 07:24:35 +0200

    add js for attribute

--- apimanager/atms/static/atms/js/atms.js
+++ apimanager/atms/static/atms/js/atms.js
@@ -1,5 +1,50 @@
 $(document).ready(function($) {
-    $('#info').click(function() {
-		alert("Hello World")
-	});
+    $('.runner button.forSave').click(function(e) {
+        e.preventDefault();
+        let runner = $(this).parent().parent().parent();
+        let name = $(runner).find('.atm_attribute_name').val();
+        let type = $(runner).find('.atm_attribute_type').val();
+        let value = $(runner).find('.atm_attribute_value').val();
+        $('.runner button.forUpdate').attr("disabled","disabled");
+		$('.runner button.forSave').attr("disabled","disabled");
+		$('.runner button.forDelete').attr("disabled","disabled");
+		$.post('save/attribute', {
+			'name': name,
+			'type': type,
+			'value': value,
+		}, function (response) {
+			location.reload();
+		});
+    });
+    $('.runner button.forUpdate').click(function(e) {
+            e.preventDefault();
+            let runner = $(this).parent().parent().parent();
+            let name = $(runner).find('.atm_attribute_name').val();
+            let type = $(runner).find('.atm_attribute_type').val();
+            let value = $(runner).find('.atm_attribute_value').val();
+            $('.runner button.forUpdate').attr("disabled","disabled");
+    		$('.runner button.forSave').attr("disabled","disabled");
+    		$('.runner button.forDelete').attr("disabled","disabled");
+    		$.post('updateattribute/attribute', {
+    			'name': name,
+    			'type': type,
+    			'value': value,
+    		}, function (response) {
+    			location.reload();
+    		});
+        });
+
+    $('.runner button.forDelete').click(function(e) {
+		e.preventDefault();
+        let runner = $(this).parent().parent().parent();
+        let atm_attribute_id = $(runner).find('.atm_attribute_id').attr("value");
+        $('.runner button.forUpdate').attr("disabled","disabled");
+		$('.runner button.forSave').attr("disabled","disabled");
+		$('.runner button.forDelete').attr("disabled","disabled");
+		$.post('delete/attribute', {
+			'atm_attribute_id': atm_attribute_id
+		}, function (response) {
+			location.reload();
+		});
+    });
 });
