9c52732164aa9eb518fcfdc3a5e68e65cfb7c169
tawoe <weserwueter@posteo.de>
2023-04-17 14:16:09 +0200

    create container pipeline for OC

---
+++ .github/local_settings_container.py
@@ -0,0 +1,28 @@
+import os
+
+if os.getenv('OAUTH_CONSUMER_KEY'):
+	OAUTH_CONSUMER_KEY = os.getenv('OAUTH_CONSUMER_KEY')
+else:
+	OAUTH_CONSUMER_KEY = "initial_migration_dummy_value"
+if os.getenv('OAUTH_CONSUMER_SECRET'):
+	OAUTH_CONSUMER_SECRET = os.getenv('OAUTH_CONSUMER_SECRET')
+else:
+	OAUTH_CONSUMER_SECRET = "initial_migration_dummy_value"
+if os.getenv('SECRET_KEY'):
+	SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+	SECRET_KEY = "initial_migration_dummy_value"
+if os.getenv('API_HOST'):
+	API_HOST = os.getenv('API_HOST')
+if os.getenv('API_PORTAL'):
+	API_PORTAL = os.getenv('API_PORTAL')
+if os.getenv('ALLOWED_HOSTS'):
+	ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
