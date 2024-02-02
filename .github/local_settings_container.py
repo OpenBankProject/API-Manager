9fa9f9e6cd62113dc273ac0acc311ca954c06b0e
tesobe-daniel <daniel@tesobe.com>
2024-02-02 15:19:06 +0100

    Add CALLBACK_BASE_URL to local_settings_container.py

--- .github/local_settings_container.py
+++ .github/local_settings_container.py
@@ -16,6 +16,8 @@ if os.getenv('API_HOST'):
 	API_HOST = os.getenv('API_HOST')
 if os.getenv('API_PORTAL'):
 	API_PORTAL = os.getenv('API_PORTAL')
+if os.getenv('CALLBACK_BASE_URL'):
+	CALLBACK_BASE_URL = os.getenv('CALLBACK_BASE_URL')
 if os.getenv('ALLOWED_HOSTS'):
 	ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
